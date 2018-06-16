524471699bb63f7fba67af0c66f5ebe826854e5e
Qishen Zhang <qishen23@gmail.com>
2018-06-15 20:27:19 -0500

    Update some testcases and print out boolean map.

--- Src/GraphDatabaseEngine/Test/TestCase.cs
+++ Src/GraphDatabaseEngine/Test/TestCase.cs
@@ -19,6 +19,17 @@
 
         }
 
+        public void PrintBooleanMap(DomainStore store)
+        {
+            Console.WriteLine("--------------Boolean Variable Map---------------");
+            foreach (string key in store.BooleanMap.Keys)
+            {
+                bool boolean = store.BooleanMap[key];
+                Console.WriteLine(String.Format("{0}: {1}", key, boolean.ToString()));
+            }
+            Console.WriteLine("-------------------------------------------------");
+        }
+
         public List<Dictionary<string, object>> Test1(GraphDBExecutor executor)
         {
             GremlinTranslator translator = new GremlinTranslator("test.4ml");
@@ -26,6 +37,7 @@
             string query = "C(a, b)";
             Body body = translator.ParseQueryString(query);
             DomainStore store = translator.DomainStores["TestModelPlus"];
+            PrintBooleanMap(store);
             var queryResult = translator.GetQueryResult(executor, store, body, new List<string> { "a", "b" });
             return queryResult;
         }
@@ -37,6 +49,7 @@
             string query = "NodeInstanceOf(b,i)";
             Body body = translator.ParseQueryString(query);
             DomainStore store = translator.DomainStores["WebGME"];
+            PrintBooleanMap(store);
             var queryResult = translator.GetQueryResult(executor, store, body, new List<string> { "b", "i" });
             return queryResult;
         }
